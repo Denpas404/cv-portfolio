fec51ce70ee4630ab7fd73c9e4b71451b4d5d01f
D.Paaske <102860689+Denpas404@users.noreply.github.com>
2025-05-22 14:18:34 +0200

    done

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/CV-PORTFOLIO",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
